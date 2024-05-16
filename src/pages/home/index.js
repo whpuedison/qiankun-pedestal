cfe0a513c38dd9346130615508a4e03a4013d731
mashengguang <mashengguang@sunlands.com>
2024-05-16 17:51:31 +0800

    feat:首页引入飞鸟特效

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { sky } from './index.less';
+import React, { useState, useEffect, useRef } from 'react';
+import BIRDS from 'vanta/dist/vanta.birds.min';
+import { birdsBox } from './index.less';
 
 function Home() {
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(BIRDS({
+        el: myRef.current,
+        mouseControls: true,
+        touchControls: false,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1,
+        backgroundAlpha: 0.3,
+        color1: 0x1a6138,
+        color2: 0xf6f7f7,
+      }));
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className={sky}>
-      1111
-      <span className="main">222</span>
-    </div>
+    <div className={birdsBox} ref={myRef} />
   );
 }
 

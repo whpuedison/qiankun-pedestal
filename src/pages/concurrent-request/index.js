397fca7bab1f5416c761fb419df01436e8931734
mashengguang <mashengguang@sunlands.com>
2024-08-22 23:13:09 +0800

    feat:并发请求

--- src/pages/concurrent-request/index.js
+++ src/pages/concurrent-request/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const ConcurrentRequest = () => {
+    const fetchUserInfo = () => {
+        axios.get('/api/userinfo')
+        .then(resp => {
+            console.log(resp);
+        })
+    }
+
+    useEffect(() => {
+        fetchUserInfo()
+    }, [])
+
     return (
         <div>ConcurrentRequest</div>
     )
